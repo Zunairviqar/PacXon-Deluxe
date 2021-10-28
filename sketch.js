1bb0b911f08734b812a610a8f1a068630cef7fcb
Zunairviqar <66274735+Zunairviqar@users.noreply.github.com>
2021-10-28 00:20:44 -0400

    Clean images

--- sketch.js
+++ sketch.js
@@ -1,11 +1,25 @@
+// let level = [][]
+
+
+
 function preload() {
-  // bg = loadImage('assets/space.png');
+  // tile = loadImage('assets/tile3.png');
+  // tile = loadImage('assets/tile2.png');
+  tile = loadImage('assets/tile.png');
+
 }
 
 function setup() {
-  createCanvas(800,500);
+  createCanvas(760,500);
 }
 
 function draw(){
+  // background(5,5,43,255);
   background(120);
+  // image(tile, 0,0, 20,20)
+  for (let i =0; i<50; i++){
+    for(let j=0; j<30; j++){
+      image(tile,i*20,j*20,24,24)
+    }
+  }
 }
