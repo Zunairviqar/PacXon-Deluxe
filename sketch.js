107c6e9c6adfb55b9f725389dd4e7bebb188dfa1
Zunairviqar <66274735+Zunairviqar@users.noreply.github.com>
2021-10-28 00:45:23 -0400

    Clear imgs

--- sketch.js
+++ sketch.js
@@ -4,21 +4,22 @@
 
 function preload() {
   // tile = loadImage('assets/tile3.png');
-  // tile = loadImage('assets/tile2.png');
-  tile = loadImage('assets/tile.png');
+  tile = loadImage('assets/Tiles/tile2.png');
+  // Good Tiles
+  // tile = loadImage('assets/Tiles/tile.png');
 
 }
 
 function setup() {
-  createCanvas(760,500);
+  createCanvas(762,502);
 }
 
 function draw(){
   // background(5,5,43,255);
-  background(120);
+  background(0);
   // image(tile, 0,0, 20,20)
-  for (let i =0; i<50; i++){
-    for(let j=0; j<30; j++){
+  for (let i =0; i<37; i++){
+    for(let j=0; j<24; j++){
       image(tile,i*20,j*20,24,24)
     }
   }
