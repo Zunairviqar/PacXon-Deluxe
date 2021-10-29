c05d91affd259192681f3e088ab2305cf039a65b
ishmalkhalid <ishmal2000@gmail.com>
2021-10-29 00:25:05 -0400

    charac movement

--- sketch.js
+++ sketch.js
@@ -1,13 +1,18 @@
 // A 2D list to store the tiles for the game board
 let level = [];
 // Stores the images
-let tile, movingTile, player;
+let tile, movingTile, player, redGhost, blueGhost, yellowGhost, pinkGhost;
+
 let tileSize;
 
 function preload() {
   tile = loadImage('assets/Tiles/tile.png');
   movingTile = loadImage('assets/Tiles/movingTile.png');
   pacXonImg = loadImage('assets/pacXon.gif');
+  redGhost = loadImage('assets/Enemies/red-ghost.png');
+  blueGhost = loadImage('assets/Enemies/blue-ghost.png');
+  yellowGhost = loadImage('assets/Enemies/yellow-ghost.png');
+  pinkGhost = loadImage('assets/Enemies/pink-ghost.png');
 }
 
 function setup() {
@@ -35,7 +40,10 @@ function setup() {
     }
   }
 
+  //declare a new player
   player = new Player();
+  ghost = new Ghost();
+
 
 }
 
@@ -46,6 +54,8 @@ function draw(){
 
   player.display();
   player.move();
+  ghost.display();
+
 }
 
 // function to draw the tiles
@@ -61,12 +71,14 @@ function drawLevel() {
     }
   }
 }
+
 function getTile(x,y) {
   x = int(x/tileSize);
   y = int(y/tileSize);
   // console.log("tile at ", x, y, "is", level[y][x]);
   return level[y][x];
 }
+
 function modifyTile(x,y) {
   x = int(x/tileSize);
   y = int(y/tileSize);
@@ -82,9 +94,11 @@ class Player {
     this.startMovingRight = false;
     this.startMovingDown = false;
   }
+
   display(){
     image(pacXonImg, this.x, this.y, 20,20)
   }
+
   move(){
     // set up sensor positions
     this.sensorLeft = this.x-2;
@@ -94,75 +108,110 @@ class Player {
     this.middleX = this.x+tileSize/2;
     this.middleY = this.y+tileSize/2;
 
-    if (keyIsDown(68)){
-      ellipse(this.sensorRight, this.middleY,5,5)
-      let id = getTile(this.sensorRight,this.middleY);
-      if (id == 0){
-        // modifyTile(this.sensorRight,this.middleY)
-        this.startMovingDown = false;
-        this.startMovingRight = true;
-      }
-      else if(id ==1){
-          this.x +=3;
-      }
+    if (keyIsPressed == true && keyCode == 68){
+      // while(this.x != width && keyCode != 68){
+        this.currKeyCode = 68;
+
+        console.log(this.currKeyCode == 68)
+        // while (this.currKeyCode == 68 || this.x >= width) {
+        //   this.x +=3;
+        // }
     }
 
-    else if (keyIsDown(65)){
-      ellipse(this.sensorLeft, this.middleY,5,5);
-      let id = getTile(this.sensorLeft,this.middleY);
-      if (id == 0){
-        modifyTile(this.sensorLeft,this.middleY)
-      }
-      else if(id ==1){
+    if (keyIsPressed == true && keyCode == 65){
+      // while(this.x != width && keyCode != 68){
+        this.currKeyCode = 65;
+
+        if (this.currKeyCode == 65) {
           this.x -=3;
-      }
+        }
     }
+      // }
+      // ellipse(this.sensorRight, this.middleY,5,5)
+      // let id = getTile(this.sensorRight,this.middleY);
+      // if (id == 0){
+      //   // modifyTile(this.sensorRight,this.middleY)
+      //   this.startMovingDown = false;
+      //   this.startMovingRight = true;
+      // }
+      // else if(id ==1){
+      //     this.x +=3;
+      // }
+      this.x = constrain(this.x, 0, width-20);
+      this.y = constrain(this.y, 0, height-20);
 
-    else if (keyIsDown(87)){
-      ellipse(this.middleX, this.sensorTop,5,5);
-      let id = getTile(this.middleX, this.sensorTop);
-      if (id == 0){
-        modifyTile(this.middleX, this.sensorTop)
-      }
-      else if(id ==1){
-          this.y -=3;
-      }
-    }
+  // }
 
-    else if (keyIsDown(83)){
-      ellipse(this.middleX, this.sensorBottom,5,5);
-      let id = getTile(this.middleX, this.sensorBottom);
-      if (id == 0){
-        modifyTile(this.middleX, this.sensorBottom);
-        this.startMovingRight = false;
-        this.startMovingDown = true;
-      }
-      else if(id ==1){
-          this.y +=3;
-      }
     }
-    // Continously Move Right
-    if (this.startMovingRight == true){
-      this.x+=3;
-      ellipse(this.sensorRight, this.middleY,5,5)
-      let id = getTile(this.sensorRight,this.middleY);
-      if (id == 0){
-        modifyTile(this.sensorRight,this.middleY)
-      }
-    }
+  }
+
+    // if (keyIsDown(65)){
+    //   ellipse(this.sensorLeft, this.middleY,5,5);
+    //   let id = getTile(this.sensorLeft,this.middleY);
+    //   if (id == 0){
+    //     modifyTile(this.sensorLeft,this.middleY)
+    //   }
+    //   else if(id ==1){
+    //       this.x -=3;
+    //   }
+    // }
+
+    // else if (keyIsDown(87)){
+    //   ellipse(this.middleX, this.sensorTop,5,5);
+    //   let id = getTile(this.middleX, this.sensorTop);
+    //   if (id == 0){
+    //     modifyTile(this.middleX, this.sensorTop)
+    //   }
+    //   else if(id ==1){
+    //       this.y -=3;
+    //   }
+    // }
+
+    // else if (keyIsDown(83)){
+    //   ellipse(this.middleX, this.sensorBottom,5,5);
+    //   let id = getTile(this.middleX, this.sensorBottom);
+    //   if (id == 0){
+    //     modifyTile(this.middleX, this.sensorBottom);
+    //     this.startMovingRight = false;
+    //     this.startMovingDown = true;
+    //   }
+    //   else if(id ==1){
+    //       this.y +=3;
+    //   }
+    // }
+    // // Continously Move Right
+    // if (this.startMovingRight == true){
+    //   this.x+=3;
+    //   ellipse(this.sensorRight, this.middleY,5,5)
+    //   let id = getTile(this.sensorRight,this.middleY);
+    //   if (id == 0){
+    //     modifyTile(this.sensorRight,this.middleY)
+    //   }
+    // }
     
-    // Continously Move Down
-    if (this.startMovingDown == true){
-      this.y+=3;
-      ellipse(this.middleX, this.sensorBottom,5,5)
-      let id = getTile(this.middleX, this.sensorBottom);
-      if (id == 0){
-        modifyTile(this.middleX, this.sensorBottom)
-      }
-    }
+    // // Continously Move Down
+    // if (this.startMovingDown == true){
+    //   this.y+=3;
+    //   ellipse(this.middleX, this.sensorBottom,5,5)
+    //   let id = getTile(this.middleX, this.sensorBottom);
+    //   if (id == 0){
+    //     modifyTile(this.middleX, this.sensorBottom)
+    //   }
+    // }
 
-    this.x = constrain(this.x, 0, width-20)
-    this.y = constrain(this.y, 0, height-20)
+// }
+
+class Ghost {
+  constructor(){
+    this.x = random(20, width-20);
+    this.y = random(20, height-20);
+  }
+
+  display(){
+    image(redGhost, 40, 90, 20,20);
+    image(blueGhost, 660, height-40, 20,20);
+    image(yellowGhost, 203, 200, 20,20);
+    image(pinkGhost, 32, 304, 20,20);
 
   }
 }
