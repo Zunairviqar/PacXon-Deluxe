70e8ac1fc87a35448102b2c82c6b2203cd94b209
Zunairviqar <66274735+Zunairviqar@users.noreply.github.com>
2021-11-11 22:42:15 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,33 +2,36 @@
 let level = [];
 // Stores the images
 let tile, movingTile, rightPacXon, leftPacXon, upPacXon, downPacXon;
+// Variables to store the ghosts/enemies
 let redGhost, blueGhost, yellowGhost, pinkGhost;
-let home;
+// Variables to store the powerups
 let bomb, ice, bolt, slow;
-
+// Array to store all the powerups
 let powerups = [];
-
+// Store the tilesize for use throughout
 let tileSize;
+// Keeping a track of the area of an enclosed regions
 let count = 0;
-
+// Storing the count for a certain region
 let c1 = 0;
-let c2 = 0;
-
+// Storing the maxArea
 let mArea;
+// Variables to store the areas, and the coordinates for filling the enclosed regions.
 let sVals = [];
-let tc;
 let pVals = [];
 let areas = [];
-
+let tc;
+// Sertting the timer for the game and initializing it to 100
 let timer = 100;
+// declaring and initializing the levels to be kept track of
 let levels = 1;
-
-
+// For storing all the enemies in the list
 let enemy = [];
+// Keeping track of the x and y positions of the ghost
 let ghostx, ghosty;
-
+// Checking if level should be up or not and initializing to false;
 let level_up = false;
-
+// State variables to keep track of the screens
 let gamestart;
 let checkMenuclick;
 let load_level;
@@ -44,7 +47,7 @@ let checkforretry;
 let gamecomplete;
 let checkforfinish;
 let mylevel;
-
+// Variables to store all the images
 let level1;
 let level2;
 let level3;
@@ -58,31 +61,30 @@ let levelup;
 let endimg;
 let finish;
 let returnto;
-
+// Variables to store all the sounds
 let gameoversound, movingsound, clickedsound, collectionsound, collisionsound, levelupsound, movement, bg;
-
+// Declaring and initializingthe counter and max counter to calculate the percentage and keep track for the preloader
 let counter = 1;;
 let maxCounter = 33;
 
-
+// Pre Loading all the assets
+// The updateCounter parameter is passed in each loadXYZ() function to call the updateCounter function for progressing the pre-loader
 function preload() {
+  // Loading the tiles to be drawn
   tile = loadImage('assets/Tiles/tile.png', updateCounter);
   movingTile = loadImage('assets/Tiles/movingTile.png', updateCounter);
-
+  // Loading all Pac-Xon direction gifs
   rightPacXon = loadImage('assets/Paxon/right_paXon.gif', updateCounter);
   leftPacXon = loadImage('assets/Paxon/left_paXon.gif', updateCounter);
   upPacXon = loadImage('assets/Paxon/up_paXon.gif', updateCounter);
   downPacXon = loadImage('assets/Paxon/down_paXon.gif', updateCounter);
-
+  // Loading all the Ghosts/ Enemies
   redGhost = loadImage('assets/Enemies/red-ghost.png', updateCounter);
   blueGhost = loadImage('assets/Enemies/blue-ghost.png', updateCounter);
   yellowGhost = loadImage('assets/Enemies/yellow-ghost.png', updateCounter);
   pinkGhost = loadImage('assets/Enemies/pink-ghost.png', updateCounter);
-
-  // home = loadImage('assets/Screens/home.gif');
+  // Loading all the screens
   main_image = loadImage('assets/Screens/home.gif', updateCounter);
-  // main_image2 = loadImage('assets/Screens/home.png');
-  // main_image3 = loadImage('assets/Screens/home-screen.gif');
   level1 = loadImage('assets/Screens/level1.png');
   level2 = loadImage('assets/Screens/level2.png');
   level3 = loadImage('assets/Screens/level3.png');
@@ -95,12 +97,12 @@ function preload() {
   endimg = loadImage('assets/Screens/gameover.png');
   finish = loadImage('assets/Screens/congrats.png');
   returnto = loadImage('assets/Screens/returnmenu.png');
-
+  // Loading all the powerups
   bomb = loadImage('assets/Extras/redbomb.png');
   ice = loadImage('assets/Extras/ice.png');
   bolt = loadImage('assets/Extras/lightning-bolt.png');
   slow = loadImage('assets/Extras/snail.png');
-
+  // Loading all the sounds
   gameoversound = loadSound('assets/Sounds/gameover.mp3');
   movingsound = loadSound('assets/Sounds/movingsound.wav');
   clickedsound = loadSound('assets/Sounds/clicked.wav');
@@ -112,7 +114,7 @@ function preload() {
 }
 
 function setup() {
-
+  // initializing the canvas and storing a reference to it
   var canvasMain = createCanvas(760,500);
     // set the ID on the canvas element
   canvasMain.id("p5_mainCanvas");
@@ -120,6 +122,7 @@ function setup() {
   // an ID of "left"
   canvasMain.parent("#center");
 
+  // initializing all the state variables for the screens
   gamestart = false;
   checkMenuclick = false;
   load_level = false;
@@ -134,76 +137,107 @@ function setup() {
   checkforretry = false;
   gamecomplete = false;
   checkforfinish = false;
+  // initializing the value of mylevels for the levels to be accessed.
   mylevel = 1;
 
+  // making use of the local storage API and obtaining the stored value of the levels that were previously ever completed by the user
   let user_levels = window.localStorage.getItem('levelsCompleted');
+  // Checking if there was any data stored,
   if (user_levels) {
+    // If so, the data from the local storage is used, otherwised the above initalized value is used instead.
     mylevel = user_levels
   }
-
+  // Declaring the tilesize
   tileSize = 20;
+  // Populates the 2D Array with 0s
   initializeLevel();
 
+  // Places 1s at the borders of the 2D Array
   resetLevel();
   tc = 0;
   player = new Player();
   // powerup = new Powerup();
 
+  // Evening out the perlin noise
   noiseDetail(24);
+  // Looping the background music
+
   bg.loop();
+  // Setting the volume of the background music to a minimal value
   bg.setVolume(0.3);
 
 
 }
 
 function draw(){
-
+  // When this is false, the MENU or the Level Selection screen appears
   if(gamestart == false){
+    // If this if false, the MENU Screen will appear, which it will initially
     if (load_level == false){
+      // If the how to play screen is clicked, the menu screen is not shown and instead the how to play screen is shown in the else {
+      // When the how to play screen is closed, the menu screen appears again as the variable becomes false
       if (loadhowtoplay == false){
+        // Clicks for the menu screen are detected
         checkMenuclick = true;
+        // The Start Screen is shown
         StartScreen();
       }
       else{
+        // The How To Play Screen is shown
         HowToPlayScreen();
+        // Clicks for that screen are detected
         checkhowtoplay = true;
       }
     }
+    // The Load Screen will appear instead of the Menu Screen
     else if(load_level == true){
+      // When the load screen is loaded, the clicks for the Menu Screen are not detected
       checkMenuclick = false;
+      // The Level Screen is showns
       LevelScreen();
+      // Clicks for the Level Screen are detected after setting the next variable to true
       checkforselectlevel = true;
     }
   }
+  // If the game start is true, the menu or any of the initial screens are not appearing
   else {
+    // Fills the background with a black color
     background(0);
+    // Draws the level, which in the first instance only draws the borders
     drawLevel();
 
+    // If the game gets completed,
     if(gamecomplete == true){
+      // The game complete screen is shown
       image(finish, 0, 0);
+      // CLicks for that screen are detected
       checkforfinish = true;
     }
     else{
+      // If the game ends
       if(endscreen == true){
+        // The game end screen is shown and
         image(endimg, 0, 0);
+        // Clicks for that screen are detected
         checkforretry =  true;
       }
       else{
+        // If the level gets incremented,
         if(levelupscreen==true){
+          // The level up screen is shown
           image(levelup, 0,0);
+          // CLicks for that screen are detected
           checkfornextLevel = true;
         }
         else{
+          // If the game has not begun yet, the click to start screen appears
           if(gamebegin==false){
             image(clicktostart, 0, 0);
+            // Clicks for that screen are detected
             checkforStart = true;
           }
-
           if(gamebegin == true){
-            // stroke(0);
-            // fill(255);
-            // text("Lives: " + player.lives, 10, 15);
-
+            // Shows the updated Lives on the HTML Page
             let window_score = document.getElementById('current_lives')
             window_score.innerHTML = player.lives;
 
@@ -222,13 +256,12 @@ function draw(){
               enemy[i].move();
             }
 
-            // gameOver();
-
+            // Shows the updated Progress on the HTML Page
             let window_progress = document.getElementById('current_progress')
             window_progress.innerHTML = completeLevel() + "%";
 
             nextLevel();
-            // text("Level: " + levels, width-60, 15);
+            // Shows the updated Levels on the HTML Page
             let window_level = document.getElementById('current_level')
             window_level.innerHTML = levels;
 
@@ -241,31 +274,43 @@ function draw(){
 
             stroke(0);
             fill(255);
-//             text("Timer: " + timer + 's', width-300, 15);
+            // Shows the updated Timer on the HTML Page
             let window_timer = document.getElementById('current_timer');
             window_timer.innerHTML = timer + 's';
-            // console.log("timer");
-
+            // Decreases the timer every second until the timer is 0
             if (frameCount % 60 == 0 && timer > 0) {
               timer --;
             }
 
+            // If the timer or the player lives become 0, the game ends!
             if (timer == 0 || player.lives == 0){
+              // The timer and the lives are updated on the HTML Page
               let window_score = document.getElementById('current_lives')
               window_score.innerHTML = player.lives;
               let window_timer = document.getElementById('current_timer');
               window_timer.innerHTML = timer + 's';
+              // The game end screen is trigerred
               endscreen = true;
+              // The right image for the Pac Xon is loaded
               player.graphic = rightPacXon;
+              // The direction and movement of the Pac-Xon is reset
               player.currKeyCode = 0;
+              // The pacXon is repositioned at the first index of the array
               player.x = 0;
               player.y = 0;
-              levels = 1;
+              // The levels are reset
+              // levels = 1;
+              // powerups are emptied
               powerups = [];
+              // The level is reset and only the borders are drawn
               resetLevel();
+              // The lives of the player are reset
               player.lives = 3;
+              // The timer is reset
               timer = 100;
+              // The game over sound is played
               gameoversound.play();
+              // The all levels function is called to choose the level
               allLevels();
             }
 
