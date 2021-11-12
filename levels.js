99f732184e183d92fc5cd744d88897769e7e8a42
ishmalkhalid <ishmal2000@gmail.com>
2021-11-11 23:20:20 -0500

    comments

--- levels.js
+++ levels.js
@@ -1,3 +1,4 @@
+// calculate and return the percentage of solid tiles in the array
 function completeLevel() {
 	let count = 0
 	let totalcount = 0;
@@ -8,38 +9,42 @@ function completeLevel() {
 	      }
 	    }
 	}
-	// count -= 122
 	totalcount = ((count/828)* 100);
-	// console.log(totalcount);
-	// totalcount = count/
 	return round(totalcount * 100) / 100;
-	// console.log(count)
-	// return totalcount;
 }
 
+// promotes player to next level
 function nextLevel() {
-	// completeLevel();
-	// levelOne();
-	if (completeLevel() >= 70) {
-		// console.log(completeLevel())
+	// if 80% of level is completed
+	if (completeLevel() >= 80) {
+		// play level up sound
 		levelupsound.play();
+		// increment level
 		levels +=1;
+		// if all 6 levels completed, 
 		if(levels>6){
+			// game has been completed
 			gamecomplete = true;
 		}
+		// else increment the number of levels unlocked
 		else{
 			mylevel +=1;
+			// set the number of levels unlocked in local storage
 			window.localStorage.setItem('levelsCompleted', levels);
 		}
 		console.log("level up")
+		// reset level, remove all walls except borders
 		resetLevel();
+		// reset player position, lives, graphics, timer
 		player.x = 0;
 		player.y = 0;
 		player.lives = 3;
 		player.graphic = rightPacXon;
 		player.currKeyCode = 0;
 		timer = 100;
+		// call the next level
 		allLevels();
+		// display the screens accordingly
 		checkfornextLevel = false;
 		levelupscreen = true;
 		gamebegin=false;
@@ -48,38 +53,39 @@ function nextLevel() {
 		checkforStart = false;
 	}
 }
-
+// function which contains all levels
 function allLevels() {
-
+	// level 2
 	if (levels == 2) {
 		levelTwo();
 	}
-
+	// level 3
 	else if (levels == 3) {
 		levelThree();
 	}
-
+	// level 4
 	else if (levels == 4) {
 		levelFour();
 	}
-
+	// level 5
 	else if (levels == 5) {
 		levelFive();
 	}
-
+	// level 6
 	else if (levels == 6) {
 		levelSix();
 	}
 }
 
+// level one enemy array declaration
 function levelOne() {
 	enemy = [];
 
 	enemy.push(new PinkGhost());
 	enemy.push(new PinkGhost());
-
 }
 
+// level two enemy array declaration
 function levelTwo() {
 	enemy = [];
 
@@ -88,6 +94,7 @@ function levelTwo() {
 	enemy.push(new RedGhost());
 }
 
+// level three enemy array declaration
 function levelThree() {
 	enemy = [];
 
@@ -96,7 +103,7 @@ function levelThree() {
 	enemy.push(new RedGhost());
 	enemy.push(new YellowGhost());
 }
-
+// level four enemy array declaration
 function levelFour() {
 	enemy = [];
 
@@ -107,6 +114,7 @@ function levelFour() {
 	enemy.push(new YellowGhost());
 }
 
+// level five enemy array declaration
 function levelFive() {
 	enemy = [];
 
@@ -118,6 +126,7 @@ function levelFive() {
 	enemy.push(new YellowGhost());
 }
 
+// level six enemy array declaration
 function levelSix() {
 	enemy = [];
 
